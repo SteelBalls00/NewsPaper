be0a050b879ce764a4a621dc00403df1ece84784
SteelBalls00 <predator7151753@mail.ru>
2023-07-14 11:54:24 +0900

    change censor

--- NewsPaper/news/templatetags/censor_filter.py
+++ NewsPaper/news/templatetags/censor_filter.py
@@ -10,10 +10,13 @@ def censor(content, censor_list):
     lower_censor_list = [words.lower() for words in censor_list]           # мы приводим в нижний регистр дабы исключить заглавные буквы.
     split_content = content.split()                                        # Создаем из контента список.
 
-    for i in range(len(split_content)):                                # Каждое слово из текста приводится в нижний регистр
-        if split_content[i].lower() in lower_censor_list:              # и сравнивается с цензор-списком.
-            first_letter = split_content[i][0]
-            other_letter = split_content[i][1:]
-            replaced_word = first_letter + '*' * len(other_letter)     # Ну тут понятно, собираем слово после цензуры
-            split_content[i] = replaced_word                           # и собираем обратно в текст контента.
+    for i in range(len(split_content)):
+        if isinstance(split_content[i], str):                               # Проверка переменной на строковый тип
+            if split_content[i].lower() in lower_censor_list:               # Каждое слово из текста приводится в нижний регистр
+                first_letter = split_content[i][0]                          # и сравнивается с цензор-списком.
+                other_letter = split_content[i][1:]
+                replaced_word = first_letter + '*' * len(other_letter)      # Ну тут понятно, собираем слово после цензуры
+                split_content[i] = replaced_word                            # и собираем обратно в текст контента.
+        else:
+            continue
     return ' '.join(split_content)
