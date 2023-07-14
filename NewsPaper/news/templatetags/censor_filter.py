f83a498769ef65259948c2c963f2d83e35666cd1
SteelBalls00 <predator7151753@mail.ru>
2023-07-14 10:26:29 +0900

    change censor

--- NewsPaper/news/templatetags/censor_filter.py
+++ NewsPaper/news/templatetags/censor_filter.py
@@ -6,12 +6,12 @@ register = template.Library()
 
 @register.filter()
 def censor(content, censor_list):
-    censor_list = ['редиска', 'Редиска', 'сортировки', 'богатые', 'метод'] # Пример списка запрещенных слов который
-    lower_censor_list = [words.lower() for words in censor_list] # мы приводим в нижний регистр дабы исключить
-    split_content = content.split()                              # заглавные буквы.
+    censor_list = ['редиска', 'Редиска', 'сортировки', 'богатые', 'метод', 'Для'] # Пример списка запрещенных слов который
+    lower_censor_list = [words.lower() for words in censor_list]           # мы приводим в нижний регистр дабы исключить заглавные буквы.
+    split_content = content.split()                                        # Создаем из контента список.
 
-    for i in range(len(split_content)):                                # Каждое слово из текста также приводится
-        if split_content[i].lower() in lower_censor_list:              # в строчный вид и сравнивается с 1-м списком.
+    for i in range(len(split_content)):                                # Каждое слово из текста
+        if split_content[i].lower() in lower_censor_list:              # сравнивается с цензор-списком.
             first_letter = split_content[i][0]
             other_letter = split_content[i][1:]
             replaced_word = first_letter + '*' * len(other_letter)     # Ну тут понятно, собираем слово после цензуры
