0fed6dcdd59cf26da199e2155ae2a8845a4b40b8
SteelBalls00 <predator7151753@mail.ru>
2023-07-14 10:30:19 +0900

    change censor

--- NewsPaper/news/templatetags/censor_filter.py
+++ NewsPaper/news/templatetags/censor_filter.py
@@ -10,8 +10,8 @@ def censor(content, censor_list):
     lower_censor_list = [words.lower() for words in censor_list]           # мы приводим в нижний регистр дабы исключить заглавные буквы.
     split_content = content.split()                                        # Создаем из контента список.
 
-    for i in range(len(split_content)):                                # Каждое слово из текста
-        if split_content[i].lower() in lower_censor_list:              # сравнивается с цензор-списком.
+    for i in range(len(split_content)):                                # Каждое слово из текста приводится в нижний регистр
+        if split_content[i].lower() in lower_censor_list:              # и сравнивается с цензор-списком.
             first_letter = split_content[i][0]
             other_letter = split_content[i][1:]
             replaced_word = first_letter + '*' * len(other_letter)     # Ну тут понятно, собираем слово после цензуры
